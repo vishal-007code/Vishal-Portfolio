7719150215c1fee498ecf9147618a53cdc44091b
vishalvats <vishalvats044@gmail.com>
2023-03-02 14:15:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Contact from './component/Contact';
+import Home from './component/Home';
+import About from './component/About';
+import NavBar from './component/Navbar';
+import Experience from './component/Work';
+import Portfolio from './component/Portfolio';
+import SocialLinks from './component/SocialLinks';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar/>
+      <Home/>
+      <About/>
+      <Experience/>
+      <Portfolio/>
+      <SocialLinks/>
+      <Contact/>
+   
+    </>
+    
   );
 }
 
